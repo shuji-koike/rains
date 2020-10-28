f4dd6a9855aec97824d443e1a8b3b7c13370378c
Shuji Koike <shuji.koike@me.com>
2020-10-28 19:33:07 +0900

    [feature] 図面を全件取得する

--- extension/content_script.ts
+++ extension/content_script.ts
@@ -20,4 +20,56 @@ export async function unCaptcha() {
   input.value = text;
 }
 
+document
+  .querySelectorAll(
+    "body > table > tbody > tr > td > form:nth-child(6) > table:nth-child(30) > tbody > tr > td > table > tbody > tr:nth-child(1) > td:nth-child(1)"
+  )
+  .forEach((e) => {
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.addEventListener("change", (e) => {
+      document.getElementsByName("bkknId1").forEach((e) => {
+        if (e instanceof HTMLInputElement) e.checked = input.checked;
+      });
+    });
+    e.appendChild(input);
+  });
+
+document.getElementsByName("zmnbtn_lower").forEach((e) => {
+  const button = document.createElement("button");
+  button.type = "button";
+  button.innerText = "図面を全件取得する";
+  button.addEventListener("click", () => {
+    const inputs = [
+      ...document.getElementsByName("bkknId1"),
+      ...document.getElementsByName("bkknId2"),
+    ]
+      .filter(isHTMLInputElement)
+      .filter((e) => e.checked);
+    for (let i = 0; i < inputs.length; i += 20) {
+      download("/reins/bkkn/KG010_004.do", {
+        bkknId: inputs.slice(i, i + 20).map((e) => e.value),
+        bkknBngu: inputs
+          .slice(i, i + 20)
+          .map((e) => e.parentElement?.querySelector("input[name=bkknBngu1]"))
+          .filter(isHTMLInputElement)
+          .map((e) => e.value),
+        kduFlg: "1",
+        knskFlg: Array(inputs.slice(i, i + 20).length).fill("0"),
+        r: Math.random().toString().substring(2),
+      });
+    }
+  });
+  e.parentElement?.append(button);
+});
+
+function download(url: string, data: Record<string, string | string[]>) {
+  const params = new URLSearchParams();
+  for (const [key, value] of Object.entries(data))
+    params.set(key, Array.isArray(value) ? value.join(",") : value);
+  window.open(url + "?" + params.toString());
+}
+
+function isHTMLInputElement(x: unknown): x is HTMLInputElement {
+  return x instanceof HTMLInputElement;
 }
