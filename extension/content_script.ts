83aa68192951bdf9a003960097f3349ef988b9ef
Shuji Koike <shuji.koike@me.com>
2020-10-28 19:30:47 +0900

    [refactor] unCaptcha

--- extension/content_script.ts
+++ extension/content_script.ts
@@ -9,22 +9,15 @@ export async function unCaptcha() {
     "img[src='/reins/ktgyoumu/KG001_001JcaptchaAction.do']"
   );
   if (!(img instanceof HTMLImageElement)) return;
+  const canvas = document.createElement("canvas");
+  canvas.getContext("2d")?.drawImage(img, 0, 0);
   const {
     data: { text },
-  } = await Tesseract.recognize(getImageData(img), "eng");
+  } = await Tesseract.recognize(canvas, "eng");
   if (!/(\d{6})/.test(text)) return;
   const input = document.querySelector("input[name='kywrd']");
   if (!(input instanceof HTMLInputElement)) return;
   input.value = text;
 }
 
-export function getImageData(img: HTMLImageElement) {
-  const canvas = document.createElement("canvas");
-  canvas.width = img.width;
-  canvas.height = img.height;
-  canvas.getContext("2d")?.drawImage(img, 0, 0);
-  return new Buffer(
-    canvas.toDataURL("image/png").replace("data:image/png;base64,", ""),
-    "base64"
-  );
 }
