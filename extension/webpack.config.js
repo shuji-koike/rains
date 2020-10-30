fef381345500f85f2d4085dbc1bc6b6f48191f4b
Shuji Koike <shuji.koike@me.com>
2020-10-31 01:22:05 +0900

    setup webpack-extension-reloader

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -1,9 +1,11 @@
-const webpack = require("webpack");
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ExtensionReloader = require("webpack-extension-reloader");
 
 module.exports = {
   mode: process.env.NODE_ENV || "development",
   entry: {
+    background: path.join(__dirname, "background.ts"),
     content_script: path.join(__dirname, "content_script.ts"),
   },
   output: {
@@ -22,5 +24,18 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
   },
-  plugins: [],
+  plugins: [
+    new ExtensionReloader({
+      reloadPage: false,
+      manifest: path.resolve(__dirname, "manifest.json"),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "manifest.json"),
+        },
+      ],
+    }),
+  ],
+  devtool: "cheap-module-source-map",
 };
